a5133c914d188707251661a66e271e916a2faa39
koin <kangyueluo777@gmail.com>
2019-08-09 18:44:44 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'FontsPage.dart';
+
+const String words1 = "Almost before we knew it, we had left the ground.";
+const String words2 = "A shining crescent far beneath the flying vessel.";
+const String words3 = "A red flair silhouetted the jagged edge of a wing.";
+const String words4 = "Mist enveloped the ship three hours out from port.";
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Fonts',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+
+      home:new FontsPage(),
+    );
+  }
+}
+
